1e106ad35165ccc30276185e3e246baa25a61fa6
Saurabh Srivastava <saurabh18srivastava1@gmail.com>
2024-11-12 21:22:14 +0530

    profile

--- app.js
+++ app.js
@@ -17,6 +17,10 @@ app.get('/', (req,res) =>{
 app.get('/login', async(req, res)=>{
     res.render("login");
 });
+app.get('/profile', isLoggedIn, (req,res) => {
+    console.log(req.user);
+    res.render("login");
+})
 app.post('/register', async(req, res)=>{
     let {email, password, username, name, age} = req.body;
 
@@ -46,15 +50,28 @@ app.post('/login', async(req, res)=>{
     if(!user) return res.status(500).send("Something went wrong");
     
     bcrypt.compare(password, user.password, function(err, result){
-        if(result) res.status(200).send("you can login");
+        if(result) {
+            let token = jwt.sign({email:email, userid: user._id}, "shhhh");
+            res.cookie("token", token);
+            res.status(200).send("you can login");
+        }
         else res.redirect("/login");
     })
 });
 app.get('/logout', (req,res) =>{
     res.cookie("token","");
     res.redirect("/login");
-})
+});
 
-
+function isLoggedIn(req, res, next) {
+   if (req.cookies.token === "") res.send("you must be logged in");
+   else{
+    let data = jwt.verify(req.cookies.token, "shhhh");
+    req.user = data;
+   }
+   next();
+    
+   
+}
 
 app.listen(3000);
