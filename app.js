c9dba36daf8a0a25def84d3160a36f9eaa477aff
Saurabh Srivastava <saurabh18srivastava1@gmail.com>
2024-11-12 11:06:43 +0530

    register and model

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const userModel = require("./models/user");
 const cookieParser = require('cookie-parser');
+const bcrypt = require('bcrypt');
 
 app.set("view engine","ejs");
 app.use(express.json());
@@ -11,5 +12,25 @@ app.use(cookieParser());
 app.get('/', (req,res) =>{
     res.render("index");
 });
+app.post('/register', async(req, res)=>{
+    let {email, password, username, name, age} = req.body;
+
+    let user = await userModel.findOne({email});
+    if(user) return res.status(500).send("user already registered");
+    
+    bcrypt.genSalt(10, (err, salt)=>{
+        bcrypt.hash(password, salt, async(err, hash) =>{
+            let user = await userModel.create({
+                
+                    username,
+                    email,
+                    age,
+                    name,
+                    password: hash
+            
+            })
+        })
+    })
+})
 
 app.listen(3000);
